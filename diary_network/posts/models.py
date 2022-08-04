75cbb29a7f125e852c637c7fd57db78e2a215cd1
tunsmm <dragon553180@gmail.com>
2022-08-04 19:33:37 +0300

    Add new Model

--- diary_network/posts/models.py
+++ diary_network/posts/models.py
@@ -3,8 +3,21 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model() 
 
+class Group(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(unique=True)
+    description = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_posts")
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group_posts",
+                              blank=True, null=True)
+    
+    def __str__(self):
+        return str(self.id)
--- diary_network/posts/views.py
+++ diary_network/posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Group, Post
+
+
+"""
+Post.objects.all() — получить все записи модели Post
+Post.objects.get(id=1) — получить запись модели Post, у которой значение поля id равно 1. Поскольку поле id — это первичный ключ, а Django автоматически создаёт у модели свойство pk, то альтернативная запись этого же запроса будет такой: Post.objects.get(pk=1).
+Post.objects.filter(pub_date__year=1854) — запрос вернёт объекты, у которых значение года в поле pub_date равно 1854. Обратите внимание на синтаксис фильтрации: двойное нижнее подчёркивание между названиями поля и фильтра. Подробнее о функции filter() — в документации.
+Post.objects.filter(text__startswith="Писать не хочется") — пример фильтра по текстовому полю, он вернёт записи, начинающиеся с указанной в фильтре строки.
+"""
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")
+    return render(request, "group.html", {"group": group, "posts": posts})
