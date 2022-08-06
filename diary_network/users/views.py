d0889533e39d8337f767cdbf00f839ac1ecdf585
tunsmm <dragon553180@gmail.com>
2022-08-06 16:12:12 +0300

    Add Validation forms

--- diary_network/users/views.py
+++ diary_network/users/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-from .forms import CreationForm
+from .forms import ContactForm, CreationForm
 
 
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+@login_required
+def user_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            return redirect('/thank-you/')
+
+        return render(request, 'contact.html', {'form': form})
+
+    form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
