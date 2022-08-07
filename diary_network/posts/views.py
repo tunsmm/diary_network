ca0150623e379617bfc855ad98a659c0405ab521
tunsmm <dragon553180@gmail.com>
2022-08-07 10:53:42 +0300

    Add view for new post

--- diary_network/posts/views.py
+++ diary_network/posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import PostForm
 from .models import Group, Post
 
 
@@ -23,3 +24,20 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")
     return render(request, "group.html", {"group": group, "posts": posts})
+
+
+@login_required
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+
+        return render(request, 'new_post.html', {'form': form})
+
+    form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
