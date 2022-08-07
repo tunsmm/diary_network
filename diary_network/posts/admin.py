23eff116085d33d5ecdc728706136b7395885c81
tunsmm <dragon553180@gmail.com>
2022-08-07 10:47:24 +0300

    Add Group model for admin view

--- diary_network/posts/admin.py
+++ diary_network/posts/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Group, Post
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "description", "slug")
+    search_fields = ("title", "description" )
+    list_filter = ("title",)
+    empty_value_display = "-empty-"
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "pub_date", "author")
+    list_display = ("pk", "text", "pub_date", "author", "group")
     search_fields = ("text", )
     list_filter = ("pub_date", "author")
     empty_value_display = "-empty-"
 
 
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Post, PostAdmin)
