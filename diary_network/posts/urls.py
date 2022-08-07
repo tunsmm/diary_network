0744e3dbf9a9e0d33b7141207f8b3f730c7717b4
tunsmm <dragon553180@gmail.com>
2022-08-07 10:52:59 +0300

    Add new route

--- diary_network/posts/urls.py
+++ diary_network/posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/<slug:slug>/", views.group_posts, name="group_posts"),
+    path("group/<slug:slug>/", views.group_posts, name="group_post"),
+    path("new/", views.new_post, name="new_post"),
 ]
