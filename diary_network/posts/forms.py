3c5e99a78406c55dc0d87d70a5ca502a1e97d20d
tunsmm <dragon553180@gmail.com>
2022-08-07 10:55:55 +0300

    Add Group and Post forms

---
+++ diary_network/posts/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm
+
+from .models import Group, Post
+
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ['title', 'slug', 'description']
+        labels = {
+            'title': ('Название'),
+            'slug': ('Уникальный URL'),
+            'description': ('Описание группы'),
+        }
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['group', 'text', ]
+        labels = {
+            'group': ('Группа'),
+            'text': ('Текст'),
+        }
