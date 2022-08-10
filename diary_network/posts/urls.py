eb6ac8b3543e5f6775aeb3129fede376b12b2432
tunsmm <dragon553180@gmail.com>
2022-08-10 23:14:33 +0300

    Add route for user profile, view post, edit post

--- diary_network/posts/urls.py
+++ diary_network/posts/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("group/<slug:slug>/", views.group_posts, name="group_post"),
-    path("new/", views.new_post, name="new_post"),
+    path("new/", views.post_new, name="post_new"),
+    path('<str:username>/', views.profile, name='profile'),
+    path('<str:username>/<int:post_id>/', views.post_view, name='post'),
+    path('<str:username>/<int:post_id>/edit/', views.post_edit, name='post_edit'),
 ]
