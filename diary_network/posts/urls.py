eb81420122a37492105f82db7e04aea0ba482398
tunsmm <dragon553180@gmail.com>
2022-08-17 12:02:59 +0300

    Add routes for Comments

--- diary_network/posts/urls.py
+++ diary_network/posts/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('<str:username>/', views.profile, name='profile'),
     path('<str:username>/<int:post_id>/', views.post_view, name='post'),
     path('<str:username>/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    
+    path("<username>/<int:post_id>/comment/", views.comment_add, name="comment_add"),
+    path("<username>/<int:post_id>/comment/<int:comment_id>/delete/", views.comment_delete, name="comment_delete"),
     path('404', views.page_not_found),
     path('500', views.server_error)
 ]
